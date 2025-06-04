6db1b9194d09ad3a6d342ac01f3dad5e9f444050
Yussef Caro <yussecaro@gmail.com>
2025-06-04 16:01:12 -0500

    Actualizo build.sh para crear superusuario automáticamente

--- build.sh
+++ build.sh
@@ -6,3 +6,10 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 
 python manage.py migrate
+
+if [[ -n "$CREATE_SUPERUSER" ]]; then
+  python manage.py createsuperuser --noinput \
+    --username "$DJANGO_SUPERUSER_USERNAME" \
+    --email "$DJANGO_SUPERUSER_EMAIL"
+  echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='$DJANGO_SUPERUSER_USERNAME').update(password='$DJANGO_SUPERUSER_PASSWORD')" | python manage.py shell
+fi
