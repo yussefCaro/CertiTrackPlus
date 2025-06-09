e797e41de818832570186f82e5c106a17939639d
Yussef Caro <yussecaro@gmail.com>
2025-06-09 11:02:22 -0500

    feat(admin): agrega logo y colores personalizados, y actualiza títulos del panel de administración

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,9 +1,12 @@
-
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
+# Personalización del admin
+admin.site.site_title = "CertiTrackPlus Admin"
+admin.site.site_header = "CertiTrackPlus Administración"
+admin.site.index_title = "Panel de administración"
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -15,5 +18,6 @@ urlpatterns = [
     path('auditores/', include('documentacion_auditores.urls')),
     path('ejecucion/', include('ejecucion_auditoria.urls')),
 ]
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
