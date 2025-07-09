99d0fff37c8b7248171b229638b76f7df7f2974e
Yussef Caro <yussecaro@gmail.com>
2025-07-09 14:29:01 -0500

    rename v2 js

--- mysite/settings.py
+++ mysite/settings.py
@@ -140,7 +140,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
