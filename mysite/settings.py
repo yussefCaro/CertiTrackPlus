eb919793a78e9ade385b5ec851c2cd811265b90f
Yussef Caro <yussecaro@gmail.com>
2025-07-09 22:30:54 -0500

    prueba para 500

--- mysite/settings.py
+++ mysite/settings.py
@@ -16,7 +16,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+DEBUG = True
+
 
 ALLOWED_HOSTS = []
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
